b3201d0ed545d85b649dd8a2f9134da04d04ea6a
zaifadafaida16-alt <zaifadafaida16@gmail.com>
2026-09-02 17:11:20 +0100

    obstacles

--- laser.py
+++ laser.py
@@ -1,5 +1,5 @@
 import pygame
-
+ 
 class Laser(pygame.sprite.Sprite):
     def __init__(self, position, speed, screen_height):
         super().__init__()
@@ -8,8 +8,8 @@ class Laser(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center = position)
         self.speed = speed
         self.screen_height = screen_height
-
-    
+ 
+   
     def update(self):
         self.rect.y -= self.speed
         if self.rect.y > self.screen_height + 15 or self.rect.y < 0:
--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import pygame, sys
-from game import game
+from game import Game
+from spaceship import Spaceship
+from obstacle import Obstacle
 pygame.init()
-
-
+ 
 SCREEN_WIDTH = 500 # screen size
-SCREEN_HEIGHT = 500 #screen height
-
-
-
+SCREEN_HEIGHT = 500
+ 
+ 
 GREY = (29, 29, 27)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Python Space Invaders")
-
+ 
 clock = pygame.time.Clock()
-
-game = game(SCREEN_WIDTH, SCREEN_HEIGHT)
-
+game = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
+ 
+spaceship = Spaceship(SCREEN_WIDTH, SCREEN_HEIGHT)
+spaceship_group = pygame.sprite.GroupSingle()
+spaceship_group.add(spaceship)
+ 
+obstacle = Obstacle(x=100, y=SCREEN_HEIGHT - 100) 
+ 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+ 
     # updating spaceship
     game.spaceship_group.update()
-
+    spaceship_group.update()
+   
+ 
     # Drawing
     screen.fill(GREY)
     game.spaceship_group.draw(screen)
     game.spaceship_group.sprite.lasers.draw(screen)
+    
     for obstacle in game.obstacles:
         obstacle.block_group.draw(screen)
-pygame.display.update()
-clock.tick(60)
-
-
+    pygame.display.update()
+    clock.tick(60)
+ 
+ 
