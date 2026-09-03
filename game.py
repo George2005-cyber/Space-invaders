91fdfcebda689dd00ad39368393212e9b9aa2c53
zaifadafaida16-alt <zaifadafaida16@gmail.com>
2026-09-03 12:12:00 +0100

    game update

--- game.py
+++ game.py
@@ -1,7 +1,10 @@
-import pygame 
+import pygame, random 
 from spaceship import Spaceship
 from obstacle import Obstacle
 from obstacle import grid
+from aliens import Alien
+from laser import Laser
+from aliens import Mysteryship
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
@@ -11,8 +14,16 @@ class Game:
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.spaceship_group = pygame.sprite.GroupSingle()
+        self.spaceship_lasers_group = pygame.sprite.Group()
         self.spaceship_group.add(Spaceship(self.screen_width, self.screen_height))
         self.obstacles = self.create_obstacle()
+        self.alien_group = pygame.sprite.Group()
+        self.create_aliens()
+        self.alien_direction = 1
+        self.alien_lasers_group = pygame.sprite.Group()
+        self.mysteryship_group = pygame.sprite.Group()
+        self.lives = 3
+        
 
     def create_obstacle(self):
         obstacle_width = len(grid[0]) * 3
@@ -23,3 +34,79 @@ class Game:
             obstacle = Obstacle(offset_x, self.screen_height - 100)
             obstacles.append(obstacle)
         return obstacles
+
+    def create_aliens(self):
+        for row in range(5):
+            for column in range(11):
+                x = 10 + column * 40
+                y = 20 + row * 40
+
+                if row == 0:
+                     alien_type = 3
+                elif row in (1, 2):
+
+                   alien_type = 2
+                else:
+                    alien_type = 1
+
+                alien = Alien(alien_type, x, y)
+                self.alien_group.add(alien)
+    def move_aliens(self):  
+        self.alien_group.update(self.alien_direction)       
+
+        alien_sprites = self.alien_group.sprites()
+        edge_hit = False
+        for alien in alien_sprites:
+            if alien.rect.right >= self.screen_width or alien.rect.left <= 0:
+                edge_hit = True
+            if edge_hit:
+                self.alien_direction *= -1
+                self.alien_shoot_down(2)
+                break
+    def alien_shoot_down(self, distance):
+        for alien in self.alien_group.sprites():
+            alien.rect.y += distance
+    def alien_shoot_laser(self):
+        if self.alien_group.sprites():
+            random_alien = random.choice(self.alien_group.sprites())
+            laser_sprite = Laser(random_alien.rect.center, -6, self.screen_height)
+            self.alien_lasers_group.add(laser_sprite)
+
+    def create_mystery_ship(self):
+             self.mysteryship_group.add(Mysteryship(self.screen_width))
+
+    def check_for_collision(self):
+         #spaceship
+         if self.spaceship_group.sprite.lasers:
+             for laser_sprite in self.spaceship_group.sprite.lasers:
+              if pygame.sprite.spritecollide(laser_sprite, self.alien_group, True):
+                    laser_sprite.kill()
+              if pygame.sprite.spritecollide(laser_sprite, self.mysteryship_group, True):
+                    laser_sprite.kill()
+              for obstacle in self.obstacles:
+                    if pygame.sprite.spritecollide(laser_sprite, obstacle.block_group, True):
+                        laser_sprite.kill()
+             #Alien lasers
+         if self.alien_group:
+                for alien_laser in self.alien_lasers_group:
+                    if pygame.sprite.spritecollide(alien_laser,self.spaceship_group, False):
+                        alien_laser.kill()
+                        self.lives -= 1
+                        print(f"Hit! Lives remaining: {self.lives}")
+                        if self.lives <= 0:
+                            print("Game Over!")
+                            pygame.quit()
+                            exit()
+                    for obstacle in self.obstacles:
+                        if pygame.sprite.spritecollide(alien_laser, obstacle.block_group, True):
+                            alien_laser.kill()    
+               #Aliens hitting barries
+         if self.alien_group:
+                for alien in self.alien_group:
+                    for obstacle in self.obstacles:
+                        pygame.sprite.spritecollide(alien, obstacle.block_group, True)
+                    if pygame.sprite.spritecollide(alien, self.spaceship_group, False):
+                        print("Game Over! Aliens reached the spaceship.")
+                        pygame.quit()
+                        exit()
+         
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import pygame, sys
+import pygame, sys, random
 from game import Game
 from spaceship import Spaceship
 from obstacle import Obstacle
+from laser import Laser
 pygame.init()
  
 SCREEN_WIDTH = 500 # screen size
@@ -20,25 +21,43 @@ spaceship_group = pygame.sprite.GroupSingle()
 spaceship_group.add(spaceship)
  
 obstacle = Obstacle(x=100, y=SCREEN_HEIGHT - 100) 
+
+SHOOT_LASER = pygame.USEREVENT
+pygame.time.set_timer(SHOOT_LASER, 300)
+
+MYSTERYSHIP = pygame.USEREVENT + 1
+pygame.time.set_timer(MYSTERYSHIP, random.randint(12000, 24000)) 
  
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        
+        if event.type == SHOOT_LASER:
+            game.alien_shoot_laser()
+        if event.type == MYSTERYSHIP:
+            game.create_mystery_ship()
+            pygame.time.set_timer(MYSTERYSHIP, random.randint(12000, 24000))  
  
     # updating spaceship
-    game.spaceship_group.update()
-    spaceship_group.update()
-   
+        game.update() # game update
+        game.spaceship_group.update()
+        spaceship_group.update()
+        game.move_aliens()
+        game.alien_lasers_group.update()
+        game.mysteryship_group.update()
+        game.check_for_collision()
  
     # Drawing
     screen.fill(GREY)
     game.spaceship_group.draw(screen)
     game.spaceship_group.sprite.lasers.draw(screen)
-    
     for obstacle in game.obstacles:
         obstacle.block_group.draw(screen)
+    game.alien_group.draw(screen)
+    game.alien_lasers_group.draw(screen)
+    game.mysteryship_group.draw(screen)
     pygame.display.update()
     clock.tick(60)
  
