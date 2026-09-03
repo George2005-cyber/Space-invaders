c0e31086c57cadf4500e4d658dde722fd85fe6fc
zaifadafaida16-alt <zaifadafaida16@gmail.com>
2026-09-03 11:00:47 +0100

    changed mystership location

---
+++ aliens.py
@@ -0,0 +1,32 @@
+import pygame 
+import random
+
+class Alien(pygame.sprite.Sprite):
+    def __init__(self,type, x, y):
+        super().__init__()
+        self.type = type
+        path = f"images/alien_{type}.png"
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+    def update(self, direction):
+        self.rect.x += direction
+
+class Mysteryship(pygame.sprite.Sprite):
+    def __init__(self, screen_width):
+        super().__init__()
+        self.screen_width = screen_width
+        self.image = pygame.image.load("images/mysteryship.png")
+        x = random.choice([0, self.screen_width - self.image.get_width()])
+        if x == 0:
+           self.speed = 3
+        else:
+           self.speed = -3
+        self.rect = self.image.get_rect(topleft=(x, 40))
+
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.right >self.screen_width:
+            self.kill()
+        elif self.rect.left < 0:
+            self.kill()
